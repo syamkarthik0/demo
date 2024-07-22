070de16443ddaf9ea2d812327cedef930f7cf52a
syamkarthik0 <113086856+syamkarthik0@users.noreply.github.com>
2024-07-22 19:07:40 +0530

    Update Untitled-1.java

--- Untitled-1.java
+++ Untitled-1.java
@@ -6,3 +6,17 @@ datatype variablename=value;
 
 float=123f; //indicate f 
 
+
+
+public class Person {
+    public String firstName;
+    
+    // Method to set the first name
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+     // Assigning value directly to field
+        person.firstName = "John";
+        
+        // Calling method to set the field value
+        person.setFirstName("Doe");
