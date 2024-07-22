fa1b4942aedda796c03cbe5dceaa8ad40f03d387
syamkarthik0 <113086856+syamkarthik0@users.noreply.github.com>
2024-07-22 19:20:21 +0530

    Update Untitled-1.java

--- Untitled-1.java
+++ Untitled-1.java
@@ -1,3 +1,5 @@
+
+
 int a; //declaration
 a=10; //initialization
 
@@ -20,3 +22,23 @@ public class Person {
         
         // Calling method to set the field value
         person.setFirstName("Doe");
+
+
+Scanner sc = new Scanner(System.in);
+    System.out.print("Enter first name: ");
+    String firstName = sc.nextLine();
+
+    System.out.print("Enter age: ");
+    int age = sc.nextInt();
+    sc.nextLine(); // After reading other types of input (like int, double, etc.),then to read a line of text input from the user    
+
+    System.out.print("Enter address: ");
+    String address = scanner.nextLine();
+    Person person = new Person(firstName, age, address);
+
+
+#Default constructor 
+    public Person {
+}
+    person p = new Person("",0); // Initial values 
+
